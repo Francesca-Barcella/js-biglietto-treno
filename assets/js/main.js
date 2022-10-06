75fbb993edab8ae5f0dd72e91e13e75da6a30db1
Francesca-Barcella <pollon.fra@gmail>
2022-10-06 14:16:45 +0200

    TT - scomposizione della traccia

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,30 @@
-alert ('funziona')
+/**************************************************
+ 
+TRACCIA ESERCIZIO
+Istruzioni
+Il programma dovrà chiedere all'utente:
+il numero di chilometri che vuole percorrere
+e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+Questo richiederà un minimo di ricerca, oltre alla ricerca da fare per ricordarsi come calcolare uno sconto (Googlate qulcosa del tipo: come si calcola lo sconto? e cercate una risorsa che vi possa aiutare a capire cosa fare)
+
+***************************************************/
+//INFOMRAZIONI DA RICHIEDERE ALL'UTENTE:
+// 1°  - chiedere all'utente quanti km deve percorrere
+
+// 2°  - chiedere all'utente quanti anni ha
+
+// CALCOLO DEL COSTO DEL VIAGGIO
+// 1° - calcolare il prezzo totale moltiplicando i km per 0.21€/km
+
+// 2° - calcolare lo sconto del 20%
+
+// 3° - calcolare lo sconto del 40%
+
+// 4° - se il passeggero ha - di 18 anni applicare sconto del 20%
+
+// 5° - altrimenti se il passeggero ha + di 65 applicare lo sconto del 40%
